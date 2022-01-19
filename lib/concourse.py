482b8e0f6e3c5022152980a821719690d69d8f15
Tobias Macey <tmacey@mit.edu>
2022-01-19 13:17:24 -0500

    Applying formatting changes for existing code

--- lib/concourse.py
+++ lib/concourse.py
@@ -1,93 +1,92 @@
 """the concourse functions and methods for the three primary commands, and their interfacing with the pulumi automation api bindings interface"""
-import sys
-import pathlib
 import json
+import pathlib
+import sys
 
 import lib.pulumi
 
 
-def check_cmd() -> str:
+def check_cmd():
     """concourse check command"""
     # assign input parameters
     params = __read_params()
 
     # read version value from stack
     version = lib.pulumi.read_stack(
-        stack_name=params['stack_name'],
-        project_name=params['project_name'],
-        output_key='version'
+        stack_name=params["stack_name"],
+        project_name=params["project_name"],
+        output_key="version",
     )
 
     # dump out json payload
-    json.dump({'version': version}, sys.stdout)
+    json.dump({"version": version}, sys.stdout)
 
 
-def in_cmd() -> str:
+def in_cmd():
     """concourse in command"""
     # assign input parameters
     params = __read_params()
     # read all outputs from stack
     outputs = lib.pulumi.read_stack(
-        stack_name=params['stack_name'],
-        project_name=params['project_name']
+        stack_name=params["stack_name"], project_name=params["project_name"]
     )
 
     # create payload with stack version and stack outputs metadata
     payload = {
-        'version': outputs['version'].value,
-        'metadata': {
-            params['stack_name']: outputs
-        },
+        "version": outputs["version"].value,
+        "metadata": {params["stack_name"]: outputs},
     }
     # TODO: dump to resource file
     json.dump(payload, sys.stdout)
 
 
-def out_cmd() -> str:
+def out_cmd():
     """concourse out command"""
     # assign input parameters
     params: dict = __read_params()
     # determine current working dir
     working_dir: str = sys.argv[1]
     # establish optional variables' default values
-    refresh_stack: bool = params.get('refresh_stack', True)
-    source_dir: str = pathlib.Path(working_dir).joinpath(params.get('source_dir', '.'))
-    stack_config: dict = params.get('stack_config', {})
+    refresh_stack: bool = params.get("refresh_stack", True)
+    source_dir: pathlib.Path = pathlib.Path(working_dir).joinpath(
+        params.get("source_dir", ".")
+    )
+    stack_config: dict = params.get("stack_config", {})
     # create pulumi stack
-    if params['action'] == 'create':
+    if params["action"] == "create":
         lib.pulumi.create_stack(
-            stack_name=params['stack_name'],
-            project_name=params['project_name'],
-            source_dir=source_dir,
-            stack_config=stack_config
-        )
-    # update pulumi stack
-    elif params['action'] == 'update':
-        lib.pulumi.update_stack(
-            stack_name=params['stack_name'],
-            project_name=params['project_name'],
+            stack_name=params["stack_name"],
+            project_name=params["project_name"],
             source_dir=source_dir,
             stack_config=stack_config,
-            refresh_stack=refresh_stack
+        )
+    # update pulumi stack
+    elif params["action"] == "update":
+        lib.pulumi.update_stack(
+            stack_name=params["stack_name"],
+            project_name=params["project_name"],
+            source_dir=source_dir,
+            stack_config=stack_config,
+            refresh_stack=refresh_stack,
         )
     # destroy pulumi stack
-    elif params['action'] == 'destroy':
+    elif params["action"] == "destroy":
         lib.pulumi.destroy_stack(
-            stack_name=params['stack_name'],
-            project_name=params['project_name'],
-            refresh_stack=refresh_stack
+            stack_name=params["stack_name"],
+            project_name=params["project_name"],
+            refresh_stack=refresh_stack,
         )
     else:
         raise RuntimeError('Invalid value for "action" parameter')
     # dump out json payload
     # TODO: determine what this should be
     payload = {
-        'version': 'placeholder',
-        'metadata': '',
+        "version": "placeholder",
+        "metadata": "",
     }
     json.dump(payload, sys.stdout)
 
 
 def __read_params(stream=sys.stdin) -> dict:
     inputs = json.load(stream)
-    return inputs['params']
+    return inputs["params"]
--- lib/pulumi.py
+++ lib/pulumi.py
@@ -6,7 +6,11 @@ def list_stack(project_name: str, runtime: str) -> list:
     """returns list of stacks for given workspace in project and runtime"""
     try:
         # select the workspace
-        workspace = automation.LocalWorkspace(project_settings=automation.ProjectSettings(name=project_name, runtime=runtime))
+        workspace = automation.LocalWorkspace(
+            project_settings=automation.ProjectSettings(
+                name=project_name, runtime=runtime
+            )
+        )
         # list the stacks in the workspace
         stacks = workspace.list_stacks()
 
@@ -19,10 +23,12 @@ def read_stack(stack_name: str, project_name: str, output_key: str = None):
     """reads output value from a specified stack"""
     try:
         # select the stack
-        stack = automation.select_stack(stack_name=stack_name,
-                                        project_name=project_name,
-                                        # no-op program, just to get outputs
-                                        program=lambda *args: None)
+        stack = automation.select_stack(
+            stack_name=stack_name,
+            project_name=project_name,
+            # no-op program, just to get outputs
+            program=lambda *args: None,
+        )
         outputs = stack.outputs()
 
         # return single value from outputs, or return all outputs
@@ -30,19 +36,23 @@ def read_stack(stack_name: str, project_name: str, output_key: str = None):
             return outputs[output_key].value
         return outputs
     except automation.StackNotFoundError as exception:
-        raise automation.StackNotFoundError(f"stack '{stack_name}' does not exist") from exception
+        raise automation.StackNotFoundError(
+            f"stack '{stack_name}' does not exist"
+        ) from exception
     except Exception as exception:
         raise Exception(str(exception)) from exception
 
 
-def create_stack(stack_name: str, project_name: str, source_dir: str, stack_config: dict) -> None:
+def create_stack(
+    stack_name: str, project_name: str, source_dir: str, stack_config: dict
+) -> None:
     """creates a stack"""
 
     try:
         # create the stack if it does not exist
-        stack = automation.create_stack(stack_name=stack_name,
-                                        project_name=project_name,
-                                        work_dir=source_dir)
+        stack = automation.create_stack(
+            stack_name=stack_name, project_name=project_name, work_dir=source_dir
+        )
         # add config kv pairs
         for config_key, config_value in stack_config.items():
             stack.set_config(config_key, automation.ConfigValue(config_value))
@@ -51,19 +61,27 @@ def create_stack(stack_name: str, project_name: str, source_dir: str, stack_conf
 
         return print(f"stack '{stack_name}' successfully created!")
     except automation.StackAlreadyExistsError as exception:
-        raise automation.StackAlreadyExistsError(f"stack '{stack_name}' already exists") from exception
+        raise automation.StackAlreadyExistsError(
+            f"stack '{stack_name}' already exists"
+        ) from exception
     except Exception as exception:
         raise Exception(str(exception)) from exception
 
 
-def update_stack(stack_name: str, project_name: str, source_dir: str, stack_config: dict, refresh_stack: bool = True) -> None:
+def update_stack(
+    stack_name: str,
+    project_name: str,
+    source_dir: str,
+    stack_config: dict,
+    refresh_stack: bool = True,
+) -> None:
     """creates or updates a stack"""
 
     try:
         # updates the stack if not already updating
-        stack = automation.select_stack(stack_name=stack_name,
-                                        project_name=project_name,
-                                        work_dir=source_dir)
+        stack = automation.select_stack(
+            stack_name=stack_name, project_name=project_name, work_dir=source_dir
+        )
         # add config kv pairs
         for config_key, config_value in stack_config.items():
             stack.set_config(config_key, automation.ConfigValue(config_value))
@@ -75,19 +93,25 @@ def update_stack(stack_name: str, project_name: str, source_dir: str, stack_conf
 
         return print(f"stack '{stack_name}' successfully updated!")
     except automation.ConcurrentUpdateError as exception:
-        raise automation.ConcurrentUpdateError(f"stack '{stack_name}' already has update in progress") from exception
+        raise automation.ConcurrentUpdateError(
+            f"stack '{stack_name}' already has update in progress"
+        ) from exception
     except Exception as exception:
         raise Exception(str(exception)) from exception
 
 
-def destroy_stack(stack_name: str, project_name: str, refresh_stack: bool = False) -> None:
+def destroy_stack(
+    stack_name: str, project_name: str, refresh_stack: bool = False
+) -> None:
     """destroys a stack"""
     try:
         # select the stack
-        stack = automation.select_stack(stack_name=stack_name,
-                                        project_name=project_name,
-                                        # no-op program for destroy
-                                        program=lambda *args: None)
+        stack = automation.select_stack(
+            stack_name=stack_name,
+            project_name=project_name,
+            # no-op program for destroy
+            program=lambda *args: None,
+        )
         # refresh the stack
         if refresh_stack:
             stack.refresh(on_output=print)
@@ -97,8 +121,12 @@ def destroy_stack(stack_name: str, project_name: str, refresh_stack: bool = Fals
 
         return print(f"stack '{stack_name}' successfully removed!")
     except automation.StackNotFoundError as exception:
-        raise automation.StackNotFoundError(f"stack '{stack_name}' does not exist") from exception
+        raise automation.StackNotFoundError(
+            f"stack '{stack_name}' does not exist"
+        ) from exception
     except automation.ConcurrentUpdateError as exception:
-        raise automation.ConcurrentUpdateError(f"stack '{stack_name}' already has update in progress") from exception
+        raise automation.ConcurrentUpdateError(
+            f"stack '{stack_name}' already has update in progress"
+        ) from exception
     except Exception as exception:
         raise Exception(str(exception)) from exception
